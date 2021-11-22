2e33716db7086dc790561782628d5800058aff18
DaviMatheus <matdavimat@gmail.com>
2021-11-21 23:10:42 -0800

    adding_todolist

---
+++ App.js
@@ -0,0 +1,105 @@
+import React, {useState} from 'react';
+import { Alert, Keyboard, KeyboardAvoidingView, Platform, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import Task from './components/Tasks';
+import { Switch } from 'react-native';
+export default function App() {
+  const [task, setTask] = useState();
+  const [taskItems, setTaskItems] = useState([]);
+
+  const handleAddTask = () => {
+    Keyboard.dismiss();
+    setTaskItems([...taskItems, task])
+  
+    setTask(null);
+  }
+
+  const compleeteTask = (index) => {
+
+    
+    let itemsCopy = [...taskItems];
+
+    itemsCopy.splice(index, 1);
+    setTaskItems(itemsCopy);
+  }
+  return (
+    <View style={styles.container}>
+
+     <View style={styles.tasksWrapper}>
+        <Text style={styles.sectionTitle}> Tarefas Atuais</Text>
+        
+        <View style={styles.items}>
+          {
+            taskItems.map((item, index) => {
+             return (
+               <TouchableOpacity key={index}  onPress={() => compleeteTask(index)}>
+                <Task text={item} />
+               </TouchableOpacity>
+             )
+            })
+          }
+        
+        </View>
+     </View>
+      
+      <KeyboardAvoidingView 
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={styles.writeTaskWrapper}
+      >
+        <TextInput style={styles.input} placeholder={'Escreva uma tarefa'} value= {task} onChangeText={text => setTask(text)}/>
+
+        <TouchableOpacity onPress={() => handleAddTask()}>
+          <View style={styles.addWrapper}>
+             <Text style={styles.addText}>+</Text>
+          </View>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
+    </View>
+  );
+}
+import colors from './assets/colors/colors'
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.background,
+  },
+  tasksWrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  items: {
+    marginTop: 30,
+  },
+  writeTaskWrapper: {
+    position: 'absolute',
+    bottom: 60,
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+  },
+  input: {
+    paddingVertical: 15,
+    paddingHorizontal: 15,
+    backgroundColor: colors.primary,
+    borderRadius: 60,
+    borderColor: colors.around,
+    borderWidth: 1,
+    width: 250,
+  },
+  addWrapper: {
+    width: 60,
+    height: 60,
+    borderRadius: 60,
+    backgroundColor: colors.primary,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderColor: colors.around,
+    borderWidth: 1,
+  },
+  addText: {},
+});
---
+++ assets/colors/colors.js
@@ -0,0 +1,11 @@
+const colors = {
+
+    background: '#E5E5E5',
+    primary: '#FFF',
+    square:  '#55BCF6',
+    around: '#C0C0C0',
+    
+
+}
+
+export default colors;
